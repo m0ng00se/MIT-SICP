2fec80eae19be49fb8357f2580a990829250ac10
m0ng00se <m0ng00se3133@yandex.com>
2015-03-31 22:41:22 -0700

    updating table scheme

--- Section-3.3/mit-scheme/exercise3-24.scm
+++ Section-3.3/mit-scheme/exercise3-24.scm
@@ -38,3 +38,35 @@
   ((table 'lookup) key))
 (define (insert! key value table)
   ((table 'insert!) key value))
+
+;;
+;; Unit Test Symbols:
+;;
+(define symbol-table (make-table equal?))
+
+(insert! 'a 10 symbol-table)
+;; ==> ok
+(insert! 'b 33 symbol-table)
+;; ==> ok
+(lookup 'a symbol-table)
+;; ==> 10
+(lookup 'b symbol-table)
+;; ==> 33
+(lookup 'c symbol-table)
+;; ==> #f
+
+;;
+;; Unit Test Numbers:
+;;
+(define number-table (make-table =))
+
+(insert! 1 'first number-table)
+;; ==> ok
+(insert! 2 'second number-table)
+;; ==> ok
+(lookup 1 number-table)
+;; ==> first
+(lookup 2 number-table)
+;; ==> second
+(lookup 3 number-table)
+;; ==> #f
