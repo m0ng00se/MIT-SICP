0419303fe51dfba9d6fd540e713cfbd3ab81843c
m0ng00se <m0ng00se3133@yandex.com>
2015-04-04 00:16:17 -0700

    updating scheme table

--- Section-3.3/mit-scheme/exercise3-25.scm
+++ Section-3.3/mit-scheme/exercise3-25.scm
@@ -6,6 +6,104 @@
 ;; keep assoc the same, and recursively induce the "lookup" and "insert!"
 ;;
 
+
+
+(define (make-table)
+  (define (assoc key records)
+    (cond ((null? records) #f)
+	  ((equal? key (caar records)) (car records))
+	  (else
+	   (assoc key (cdr records)))))
+
+  (let ((table (list '*table*)))
+    
+    ;; "lookup" procedure:
+    (define (lookup keys)
+      (define (lookup-iter key-list local-table)
+	(let ((subtable (assoc (car key-list) (cdr local-table))))
+	  (if subtable
+	      (if (list? subtable)
+		  (lookup-iter (cdr key-list) subtable)
+		  (cdr subtable))
+	      #f)))
+      (lookup-iter keys table))
+    
+    ;; "insert!" procedure:
+    (define (insert! keys value)
+      (define (make-record key-list)
+	(if (null? (cdr key-list))
+	    (cons (car key-list) value)
+	    (list (car key-list) (make-record (cdr key-list)))))	    
+      (define (insert!-iter key-list local-table)
+	(let ((subtable (assoc (car key-list) (cdr local-table))))
+	  (if subtable
+	      (if (list? subtable)
+		  (insert!-iter (cdr keys) subtable)
+		  (set-cdr! subtable value))
+	      (set-cdr! local-table
+			(cons (make-entry keys)
+			      (cdr local-table))))))
+      (insert!-iter keys table))
+
+    ;; "dispatch" procedure:
+    (define (dispatch m)
+      (cond ((eq? m 'lookup) lookup)
+	    ((eq? m 'insert!) insert)
+	    (else
+	     (error "Unknown message" m))))
+    dispatch))
+
+;;
+;; Define helper methods:
+;;
+(define (lookup keys table)
+  ((table 'lookup) keys))
+(define (insert! keys value table)
+  ((table 'insert!) keys value))
+
+;;
+;; Define specifics for "operation-table":
+;;
+(define operation-table (make-table))
+(define get (lambda (keys) (lookup keys operation-table)))
+(define put (lambda (keys value) (insert! keys value operation-table)))
+
+
+
+(define (make-table)
+  ;; "assoc" procedure:
+  (define (assoc key records)
+    (cond ((null? records) #f)
+	  ((equal? key (caar records)) (car records))
+	  (else
+	   (assoc key (cdr records)))))
+
+  (let ((table (list '*table*)))
+    
+    ;; "lookup" procedure:
+    (define (lookup keys)
+      (define (lookup-iter key-list records)
+	(let ((subtable (assoc (car key-list) (cdr records))))
+	  (if subtable 
+	      (if (list? subtable)
+		  (lookup-iter (cdr key-list) subtable)
+		  (cdr subtable))
+	      #f)))
+      (lookup-iter keys table))
+
+    ;; "insert!" procedure:
+    (define (insert! keys value)
+      '())
+
+    ;; "dispatch" procedure:
+    (define (dispatch m)
+      (cond ((eq? m 'lookup) lookup)
+	    ((eq? m 'insert!) insert!)
+	    (else
+	     (error "Unknown message" m))))
+    dispatch))
+
+
 ;;
 ;; Table Constructor:
 ;;
