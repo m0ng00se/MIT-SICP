2349947b5531102888ec1f71e944c3df61669a95
m0ng00se <m0ng00se3133@yandex.com>
2015-04-04 01:24:02 -0700

    updating scheme

--- Section-3.3/mit-scheme/exercise3-25.scm
+++ Section-3.3/mit-scheme/exercise3-25.scm
@@ -38,9 +38,10 @@
       (define (insert!-iter key-list local-table)
 	(let ((subtable (assoc (car key-list) (cdr local-table))))
 	  (if subtable
-	      (if (list? key-list)
-		  (insert!-iter (cdr key-list) subtable)
-		  (set-cdr! subtable value))
+	      (cond ((list? subtable)
+		     (insert!-iter (cdr key-list) subtable))
+		    ((pair? subtable)
+		     (set-cdr! subtable value)))
 	      (set-cdr! local-table
 			(cons (make-record key-list)
 			      (cdr local-table))))))
