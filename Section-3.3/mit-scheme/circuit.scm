1e70bbb99ffd869319fa494641e6968c594283f8
m0ng00se <m0ng00se3133@yandex.com>
2015-04-20 17:42:16 -0700

    updating scheme

--- Section-3.3/mit-scheme/circuit.scm
+++ Section-3.3/mit-scheme/circuit.scm
@@ -34,7 +34,7 @@
 
 (define (get-signal wire)
   (wire 'get-signal))
-(define (set-signal! wire value)
+(define (set-signal! wire new-value)
   ((wire 'set-signal!) new-value))
 (define (add-action! wire action-procedure)
   ((wire 'add-action!) action-procedure))
