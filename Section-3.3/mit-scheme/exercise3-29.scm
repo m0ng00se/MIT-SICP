50bce61a05b880dbc8916b462612368918c11932
m0ng00se <sholtz9421@gmail.com>
2015-10-24 20:48:20 -0700

    updating scheme, circuits

--- Section-3.3/mit-scheme/exercise3-29.scm
+++ Section-3.3/mit-scheme/exercise3-29.scm
@@ -55,6 +55,130 @@
 ;;
 
 ;;
-;; Cannot unit test the or-gate yet, since we do not have a definition 
-;; for make-wire.
+;; To unit test this, we first need to define logical-and and logical-not procedures:
 ;;
+(define (logical-and i1 i2)
+  ;; define error handler
+  (define (error-handler)
+    (error "Invalid signal" i1 i2))
+
+  ;; check to make sure i1, i2 are either 0 or 1
+  (if (and (or (= i1 0) (= i1 1))
+	   (or (= i2 0) (= i2 1)))
+
+      ;; perform logical-and
+      (cond ((and (= i1 1) (= i2 1)) 1)
+	    ((or (= i1 0) (= i2 0)) 0)
+	    (else
+	     (error-handler)))
+
+      ;; input signals are wrong
+      (error-handler)))
+
+(logical-and 1 1)
+;; ==> 1
+(logical-and 1 0)
+;; ==> 0
+(logical-and 0 1)
+;; ==> 0
+(logical-and 0 0)
+;; ==> 0
+
+(logical-and 2 1)
+;; ==> Invalid signal 2 1
+(logical-and 1 2)
+;; ==> Invalid signal 1 2
+(logical-and 2 2)
+;; ==> Invalid signal 2 2
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+	((= s 1) 0)
+	(else
+	 (error "Invalid signal" s))))
+
+(logical-not 0)
+;; ==> 1
+(logical-not 1)
+;; ==> 0 
+
+(logical-not 2)
+;; ==> Invalid signal 2
+
+;;
+;; Next we define the and-gate and the inverter:
+;;
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+	   (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+		   (lambda ()
+		     (set-signal! output new-value)))))
+
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+		   (lambda ()
+		     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+;;
+;; Define and-gate-delay and inverter-delay for unit-testing:
+;;
+(define and-gate-delay 2)
+;; ==> 2
+(define inverter-delay 3)
+;; ==> 3
+
+;;
+;; Unit test the and-gate:
+;;
+(define a1 (make-wire))
+(define a2 (make-wire))
+(define a3 (make-wire))
+
+(and-gate a1 a2 a3)
+;; ==> ok
+
+(probe 'a3 a3)
+;; ==> a3 0 New-value = 0
+
+(get-signal a1)
+;; ==> 0
+(get-signal a2)
+;; ==> 0
+(get-signal a3)
+;; ==> 0
+
+;;
+;; For an and-gate, setting one input to 1 does not change the value of the output:
+;;
+(set-signal! a1 1)
+(propagate)
+;; ==> done
+
+(set-signal! a2 2)
+(propagate)
+;; ==> a3 4 New-value = 1
+
+(get-signal a1)
+;; ==> 1
+(get-signal a2)
+;; ==> 1
+(get-signal a3)
+;; ==> 1
+
+(set-signal! a1 0)
+(propagate)
+;; ==> a3 6 New-value = 0
+
+(set-signal! a2 0)
+(propagate) 
+;; ==> done
