aabd208775a9ec5ff692e64ebad75db68ab2814f
m0ng00se <sholtz9421@gmail.com>
2015-11-23 23:03:49 -0800

    updating scheme

--- Section-3.3/mit-scheme/exercise3-30.scm
+++ Section-3.3/mit-scheme/exercise3-30.scm
@@ -1,8 +1,36 @@
 ;;
 ;; [Exercise 3.30]
 ;;
-;; [Working]
+;; Figure 3.27 shows a ripple-carry adder formed by stringing together n 
+;; full-adders. This is the simplest form of parallel adder for adding 
+;; two n-bit binary numbers. The inputs A1, A2, A3, ..., A(N) and B1, B2, 
+;; B3, ..., B(N) are the two binary numbers to be added (each A(k) and B(k) 
+;; is a 0 or a 1). The circuit generates S1, S2, S3, ..., SN, the n bits of 
+;; the sum, and C, the carry from the addition. Write a procedure 
+;; ripple-carry-adder that genreates this circuit. The procedure should
+;; take as arguments three lists of n wires each -- and A(k), the B(k) and
+;; the S(k) -- and also another wire C. The major drawback of the ripple-carry
+;; adder is the need to wait for the carry signals to propagate. What is
+;; the delay needed to obtain the complete output from an n-bit ripple-carry
+;; adder, expressed in terms of the delays for and-gates, or-gates and inverters?
 ;;
+;;        A1 B1           A2 B2           A3 B3
+;;        |  |    C1      |  |    C2      |  |    C3
+;;        |  |  +----+    |  |  +----+    |  |  +----
+;;        |  |  |    |    |  |  |    |    |  |  |
+;;      +-+--+--+-+  |  +-+--+--+-+  |  +-+--+--+-+
+;;      |         |  |  |         |  |  |         |
+;;      |         |  |  |         |  |  |         |
+;;      |         |  |  |         |  |  |         |
+;;      |         |  |  |         |  |  |         |
+;;      +-+-----+-+  |  +-+-----+-+  |  +-+-----+-+
+;;        |     |    |    |     |    |    |     |
+;;        |     |    |    |     |    |    |     |
+;; C -----+     |    +----+     |    +----+     |
+;;              S1              S2              S3
+;;
+
+;; [WORKING]
 
 ;;
 ;; Import the circuit library:
