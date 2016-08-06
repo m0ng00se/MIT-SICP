c70c6fef1e816b8bd8ce8882458fe4d7232bcd81
m0ng00se <sholtz9421@gmail.com>
2016-08-06 09:33:25 -0700

    Adding emacs exercise

---
+++ Section-2.2/emacs/exercise2-28.el
@@ -0,0 +1,40 @@
+;;
+;; Exercise 2.28
+;;
+;; Write a procedure "fringe" that takes as arguments a tree (represented as a list) and
+;; returns a list whose elements are all the leaves of the tree arranged in left-to-right
+;; order. For example,
+;;
+;; (define x (list (list 1 2) (list 3 4)))
+;;
+;; (fringe x)
+;; (1 2 3 4)
+;;
+;; (fringe (list x x))
+;; (1 2 3 4 1 2 3 4)
+;;
+
+;;
+;; Define the "fringe" procedure:
+;;
+(defun fringe (x)
+  (defun fringe-iter (y)
+    (cond ((null y) '())
+	  ((numberp y) (list y))
+	  ((listp y)
+	   (append (fringe-iter (car y))
+		   (fringe-iter (cdr y))))
+	  (t
+	   '())))
+  (fringe-iter x))
+
+;;
+;; Run some unit tests:
+;;
+(setq x (list (list 1 2) (list 3 4)))
+
+(fringe x)
+;; ==> (1 2 3 4)
+
+(fringe (list x x))
+;; ==> (1 2 3 4 1 2 3 4)
