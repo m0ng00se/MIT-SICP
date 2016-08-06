28a353613c73b20af862556def094a5d572c6d36
m0ng00se <sholtz9421@gmail.com>
2016-08-06 10:15:46 -0700

    adding exercise header

--- Section-2.2/emacs/exercise2-31.el
+++ Section-2.2/emacs/exercise2-31.el
@@ -1,3 +1,11 @@
+;;
+;; Exercise 2.31
+;;
+;; Abstract your answer from Exercise 2.30 to produce a procedure "tree-map" with the property
+;; that "square-tree" could be defined as:
+;;
+;; (define (square-tree tree) (tree-map square tree))
+;;
 
 ;;
 ;; Define the "tree-map" procedure directly:
