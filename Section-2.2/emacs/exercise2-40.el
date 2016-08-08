8a7217ba001256bf5fad831ee0e405326b8ca664
m0ng00se <sholtz9421@gmail.com>
2016-08-07 22:20:25 -0700

    Adding emacs exercise, and correcting typo in scheme

--- Section-2.2/emacs/exercise2-40.el
+++ Section-2.2/emacs/exercise2-40.el
@@ -1,29 +1,22 @@
-;;
-;; Exercise 2.40
-;;
-;; Define a procedure "unique-pairs" that, given an integer n, generates the 
-;; sequence of pairs (i,j) with 1 <= j < i <= n. Use "unique-pairs" 
-;; to simplify the definition of "prime-sum-pairs" given above.
-;;
 
 ;;
 ;; First (re-)define the "accumulate" procedure:
 ;;
-(define (accumulate op initial sequence)
-  (define (iter result rest)
-    (if (null? rest)
+(defun accumulate (op initial sequence)
+  (defun iter (result rest)
+    (if (null rest)
 	result
-	(iter (op result (car rest))
-	      (cdr rest))))
+      (iter (funcall op result (car rest))
+	    (cdr rest))))
   (iter initial sequence))
 
 ;;
 ;; Define the "enumerate-interval" procedure:
 ;;
-(define (enumerate-interval i j)
-  (define (iter count total)
+(defun enumerate-interval (i j)
+  (defun iter (count total)
     (cond ((<= count j) (iter (+ count 1) (append total (list count))))
-	  (else 
+	  (t
 	   total)))
   (iter i '()))
 
@@ -39,39 +32,39 @@
 (enumerate-interval 1 2)
 ;; ==> (1 2)
 (enumerate-interval 1 0)
-;; ==> ()
+;; ==> nil
 
 ;;
 ;; "unique-pairs" can be defined by simply abstracting the procedure
 ;; definition already given in the text, which defines the generation of pairs.
 ;;
-(define (unique-pairs n)
+(defun uniuque-pairs (n)
   (accumulate append
 	      '()
-	      (map (lambda (i)
-		     (map (lambda (j) (list i j))
-			  (enumerate-interval 1 (- i 1))))
-		   (enumerate-interval 1 n))))
+	      (mapcar (lambda (i)
+			(mapcar (lambda (j) (list i j))
+				(enumerate-interval 1 (- i 1))))
+		      (enumerate-interval i n))))
 
 ;;
 ;; This definition is workable, but as was pointed out in the text, the combination
-;; of mapping and accumulating is so common that it's best to isolate it as its 
+;; of mapping and accumulating is so common that it's best to isolate it as its
 ;; own separate procedure:
 ;;
-(define (flatmap proc seq)
-  (accumulate append '() (map proc seq)))
+(defun flatmap (proc seq)
+  (accumulate #'append '() (mapcar proc seq)))
 
 ;;
 ;; Using this abstraction, we can now write:
 ;;
-(define (unique-pairs n)
+(defun unique-pairs (n)
   (flatmap (lambda (i)
-	     (map (lambda (j) (list i j))
-		  (enumerate-interval 1 (- i 1))))
+	     (mapcar (lambda (j) (list i j))
+		     (enumerate-interval 1 (- i 1))))
 	   (enumerate-interval 1 n)))
 
 (unique-pairs 1)
-;; ==> ()
+;; ==> nil
 (unique-pairs 2)
 ;; ==> ((2 1))
 (unique-pairs 3)
@@ -80,26 +73,41 @@
 ;; ==> ((2 1) (3 1) (3 2) (4 1) (4 2) (4 3) (5 1) (5 2) (5 3) (5 4))
 
 ;;
-;; In order to implement the "prime-sum-pairs" procedure, we need to 
+;; Define helper functions:
+;;
+(defun even? (n)
+  (= (mod n 2) 0))
+(defun square (n)
+  (* n n))
+(defun filter (pred seq)
+  (cond ((null seq) '())
+	((funcall pred (car seq))
+	 (cons (car seq)
+	       (filter pred (cdr seq))))
+	(t
+	 (filter pred (cdr seq)))))
+
+;;
+;; In order to implement the "prime-sum-pairs" procedure, we need to
 ;; implement a prime-testing routine, which we will import from Section 1.2:
 ;;
-(define (expmod base exp m)
+(defun expmod (base exp m)
   (cond ((= exp 0) 1)
 	((even? exp)
-	 (remainder (square (expmod base (/ exp 2) m)) m))
-	(else
-	 (remainder (* base (expmod base (- exp 1) m)) m))))
+	 (mod (square (expmod base (/ exp 2) m)) m))
+	(t
+	 (mod (* base (expmod base (- exp 1) m)) m))))
 
-(define (prime? n)
-  (define (get-random-a)
+(defun prime? (n)
+  (defun get-random-a ()
     (+ 2 (random (- n 4))))
-  (define (test a)
+  (defun test (a)
     (= (expmod a (- n 1) n) 1))
-  (cond ((= n 2) #t)
-	((= n 3) #t)
-	((= n 4) #f)
-	((= n 5) #t)
-	(else
+  (cond ((= n 2) t)
+	((= n 3) t)
+	((= n 4) nil)
+	((= n 5) t)
+	(t
 	 (and (test (- n 1))
 	      (test (- n 2))
 	      (test (get-random-a))
@@ -107,28 +115,23 @@
 	      (test (get-random-a))))))
 
 (prime? 3)
-;; ==> #t
+;; ==> t
 (prime? 1000999)
-;; ==> #t
+;; ==> t
 
 ;;
 ;; We also need the supporting procedures defined in the text:
 ;;
-(define (prime-sum? pair)
+(defun prime-sum? (pair)
   (prime? (+ (car pair) (cadr pair))))
-(define (make-pair-sum pair)
+(defun make-pair-sum (pair)
   (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
 
 ;;
-;; With all this machinery in place, we can re-define the "prime-sum-pairs" 
+;; With all this machinery in place, we can re-define the "prime-sum-pairs"
 ;; procedure as follows:
 ;;
-(define (prime-sum-pairs n)
-  (map make-pair-sum
-       (filter prime-sum? (unique-pairs n))))
-
-;;
-;; Testing this procedure:
-;;
-(prime-sum-pairs 6)
+(defun prime-sum-pairs (n)
+  (mapcar #'make-pair-sum
+	  (filter #'prime-sum? (unique-pairs n))))
 ;; ==> ((2 1 3) (3 2 5) (4 1 5) (4 3 7) (5 2 7) (6 1 7) (6 5 11))
