c0d57d72c31ff7a128a459a99bbf2a8588d7b234
m0ng00se <sholtz9421@gmail.com>
2016-08-11 21:27:20 -0700

    updating emacs

--- Section-2.2/emacs/exercise2-52.el
+++ Section-2.2/emacs/exercise2-52.el
@@ -62,7 +62,7 @@
 	  (right (right-split painter (- n 1))))
       (let ((top-left up)
 	    (bottom-right right)
-	    (corner (corner-split paitner (- n 1))))
+	    (corner (corner-split painter (- n 1))))
 	(beside (below painter top-left)
 		(below bottom-right corner))))))
 
