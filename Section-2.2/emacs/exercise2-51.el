47d2ed7de3fa42055a90a658ba2b661d3017a1ee
m0ng00se <sholtz9421@gmail.com>
2016-08-11 20:43:14 -0700

    fixing exercise 51

--- Section-2.2/emacs/exercise2-51.el
+++ Section-2.2/emacs/exercise2-51.el
@@ -32,7 +32,7 @@
 ;; Using the second approach:
 ;;
 (defun below (painter1 painter2)
-  (rotate270 (beside (rotate90 painter2 (rotate90 painter1)))))
+  (rotate270 (beside (rotate90 painter2) (rotate90 painter1))))
 
 ;;
 ;; Sanity check:
