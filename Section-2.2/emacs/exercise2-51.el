8d6623c80b239a510d7cb98ec15ace3d5a76858d
m0ng00se <sholtz9421@gmail.com>
2016-08-11 21:10:27 -0700

    updating emacs

--- Section-2.2/emacs/exercise2-51.el
+++ Section-2.2/emacs/exercise2-51.el
@@ -1,3 +1,12 @@
+;;
+;; Exercise 2.51
+;;
+;; Define the "below" operation for painters. "Below" takes two painters as arguments. The resulting
+;; painter, given a frame, draws with the first painter in the bottom of the frame and wiht the
+;; second painter in the top. Define "below" in two different ways -- first by writing a procedure
+;; that is analogous to the "beside" procedure given above, and again in terms of "beside" and
+;; suitable rotation operations (see Exercise 2.50)
+;;
 
 ;; Load the procedures from previous exercise:
 (load-file "exercise2-50.el")
