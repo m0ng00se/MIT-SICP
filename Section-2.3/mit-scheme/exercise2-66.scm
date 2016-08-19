d56bc4f2d08af14869d757bf3da247a189481c5e
m0ng00se <sholtz9421@gmail.com>
2016-08-19 12:48:46 -0700

    Modifying scheme'

--- Section-2.3/mit-scheme/exercise2-66.scm
+++ Section-2.3/mit-scheme/exercise2-66.scm
@@ -22,9 +22,9 @@
     (if (null? tree)
 	result-list
 	(copy-to-list (left-branch tree)
-		            (cons (entry tree)
-				      (copy-to-list (right-branch tree)
-						      result-list)))))
+		      (cons (entry tree)
+			    (copy-to-list (right-branch tree)
+					  result-list)))))
   (copy-to-list tree '()))
 
 ;;
@@ -38,16 +38,16 @@
       (cons '() elts)
       (let ((left-size (quotient (- n 1) 2)))
 	(let ((left-result (partial-tree elts left-size)))
-	    (let ((left-tree (car left-result))
-		  (non-left-elts (cdr left-result))
-		  (right-size (- n (+ left-size 1))))
-	          (let ((this-entry (car non-left-elts))
-			  (right-result (partial-tree (cdr non-left-elts)
-						            right-size)))
-		          (let ((right-tree (car right-result))
-				    (remaining-elts (cdr right-result)))
-			    (cons (make-tree this-entry left-tree right-tree)
-				        remaining-elts))))))))
+	  (let ((left-tree (car left-result))
+		(non-left-elts (cdr left-result))
+		(right-size (- n (+ left-size 1))))
+	    (let ((this-entry (car non-left-elts))
+		  (right-result (partial-tree (cdr non-left-elts)
+					      right-size)))
+	      (let ((right-tree (car right-result))
+		    (remaining-elts (cdr right-result)))
+		(cons (make-tree this-entry left-tree right-tree)
+		      remaining-elts))))))))
 
 
 ;;
