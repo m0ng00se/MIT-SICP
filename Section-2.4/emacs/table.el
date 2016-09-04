8715cb2b3c1eb1c5419a3d2fc2160130ad9725f6
m0ng00se <sholtz9421@gmail.com>
2016-09-04 00:15:34 -0700

    Making progress on CLISP/emacs table

--- Section-2.4/emacs/table.el
+++ Section-2.4/emacs/table.el
@@ -3,41 +3,35 @@
 ;;
 (defun assoc (key records)
   (cond ((null records) '())
-	((equal key (car (car records)) (car records)))
+	((equal key (car (car records))) (car records))
 	(t
 	 (assoc key (cdr records)))))
 
 (defun make-table ()
   (let ((local-table (list '*table*)))
     (defun lookup (key-1 key-2)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-	(if subtable
-	    (let ((record (assoc key-2 (cdr subtable))))
-	      (if record
-		  (cdr record)
-		'()))
-	  '())))
+      '())
     (defun insert! (key-1 key-2 value)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-	(if subtable
-	    (let ((record (assoc key-2 (cdr subtable))))
-	      (if record
-		  (set-cdr! record value)
-		(set-cdr! subtable
-			  (cons (cons key-2 value)
-				(cdr subtable)))))
-	  (set-cdr! local-table
-		    (cons (list key-1
-				(cons key-2 value))
-			  (cdr local-table)))))
-      'ok)
+      '())
     (defun dispatch (m)
-      (cond ((eq m 'lookup-proc) lookup)
-	    ((eq m 'insert-proc!) insert!)
+      (cond ((eq m 'lookup-proc) #'lookup)
+	    ((eq m 'insert-proc!) #'insert!)
 	    (t
 	     (error "Unknown operation -- TABLE" m))))
-    dispatch))
+    #'dispatch))
 
-(defun operation-table (make-table))
-(defun get (operation-table 'lookup-proc))
-(defun put (operation-table 'insert-proc!))
+(defun make-table ()
+  (lexical-let ((local-table '(list *table*)))
+	       (lexical-let ((lookup (lambda (key-1 key-2)
+				       (let ((subtable
+					      'a)))
+				       (defun dispatch (m)
+					 (cond ((eq m 'lookup-proc) lookup)
+					       ((eq m 'insert-proc!) local-table)
+					       (t
+						(error "Unknown operation -- TABLE" m))))
+				       #'dispatch)))
+			    
+;;(setq operation-table (make-table))
+;;(defun get (operation-table 'lookup-proc))
+;;(defun put (operation-table 'insert-proc!))
