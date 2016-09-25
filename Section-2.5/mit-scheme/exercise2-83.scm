007cb2278339d300ed0296004559dcc05efa7044
m0ng00se <sholtz9421@gmail.com>
2016-09-24 20:51:44 -0700

    Adding missing selectors for scheme

--- Section-2.5/mit-scheme/exercise2-83.scm
+++ Section-2.5/mit-scheme/exercise2-83.scm
@@ -52,6 +52,21 @@
 
 (define (make-integer n)
   ((get 'make 'integer) n))
+
+(define (add x y)
+  (apply-generic 'add x y))
+(define (sub x y)
+  (apply-generic 'sub x y))
+(define (mul x y)
+  (apply-generic 'mul x y))
+(define (div x y)
+  (apply-generic 'div x y))
+(define (exp x y)
+  (apply-generic 'exp x y))
+(define (equ? x y)
+  (apply-generic 'equ? x y))
+(define (=zero? p)
+  (apply-generic '=zero? p))
   
 ;;
 ;; Install the integer package:
