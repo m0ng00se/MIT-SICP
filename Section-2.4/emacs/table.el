12c165f09864c5c9b7a6b02bb3195f55d1d35478
m0ng00se <sholtz9421@gmail.com>
2016-09-25 13:00:01 -0700

    Have a working table.el module

--- Section-2.4/emacs/table.el
+++ Section-2.4/emacs/table.el
@@ -1,6 +1,9 @@
 ;;
 ;; Code for handling a two-dimensional table (from Chapter 3)
 ;;
+
+(require 'cl)
+
 (defun assoc (key records)
   (cond ((null records) '())
 	((equal key (car (car records))) (car records))
@@ -8,30 +11,31 @@
 	 (assoc key (cdr records)))))
 
 (defun make-table ()
-  (let ((local-table (list '*table*)))
-    (defun lookup (key-1 key-2)
-      '())
-    (defun insert! (key-1 key-2 value)
-      '())
-    (defun dispatch (m)
-      (cond ((eq m 'lookup-proc) #'lookup)
-	    ((eq m 'insert-proc!) #'insert!)
-	    (t
-	     (error "Unknown operation -- TABLE" m))))
-    #'dispatch))
-
-(defun make-table ()
-  (lexical-let ((local-table '(list *table*)))
-	       (lexical-let ((lookup (lambda (key-1 key-2)
-				       (let ((subtable
-					      'a)))
-				       (defun dispatch (m)
-					 (cond ((eq m 'lookup-proc) lookup)
-					       ((eq m 'insert-proc!) local-table)
-					       (t
-						(error "Unknown operation -- TABLE" m))))
-				       #'dispatch)))
-			    
-;;(setq operation-table (make-table))
-;;(defun get (operation-table 'lookup-proc))
-;;(defun put (operation-table 'insert-proc!))
+  (lexical-let* ((local-table (list '*table*)))
+		(defun lookup (key1 key2)
+		  (lexical-let* ((subtable (assoc key1 (cdr local-table))))
+				(if subtable
+				    (lexical-let* ((record (assoc key2 (cdr subtable))))
+						  (if record
+						      (cdr record)
+						    '()))
+				  '())))
+		(defun insert (key1 key2 value)
+		  (lexical-let* ((subtable (assoc key1 (cdr local-table))))
+				(if subtable
+				    (lexical-let* ((record (assoc key2 (cdr subtable))))
+						  (if record
+						      (setcdr record value)
+						    (setcdr subtable
+							    (cons (cons key2 value)
+								  (cdr subtable)))))
+				  (setcdr local-table
+					  (cons (list key1
+						      (cons key2 value))
+						(cdr local-table))))))
+		(defun dispatch (m)
+		  (cond ((eq m 'lookup-proc) #'lookup)
+			((eq m 'insert-proc!) #'insert)
+			(t
+			 (error "Unknown operation -- TABLE" m))))
+		#'dispatch))
