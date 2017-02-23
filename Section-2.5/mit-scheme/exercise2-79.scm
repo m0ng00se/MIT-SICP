ca2941e9eeb401e08cb607fadae549147c226280
m0ng00se <sholtz9421@gmail.com>
2017-02-22 21:47:35 -0800

    Adding information to 79

--- Section-2.5/mit-scheme/exercise2-79.scm
+++ Section-2.5/mit-scheme/exercise2-79.scm
@@ -8,6 +8,30 @@
 
 (load "numbers.scm")
 
+;;
+;; Import the procedures we defined in Exercise 2.78:
+;;
+(define (attach-tag type-tag contents)
+  (if (number? contents)
+      contents
+      (cons type-tag contents)))
+
+(define (type-tag datum)
+  (cond ((pair? datum) (car datum))
+	((number? datum) 'scheme-number)
+	(else
+	 (error "Bad tagged datum -- TYPE-TAG" datum))))
+
+(define (contents datum)
+  (cond ((pair? datum) (cdr datum))
+	((number? datum) datum)
+	(else
+	 (error "Bad tagged datum -- CONTENTS" datum))))
+
+;;
+;; Moving on to the answer:
+;;
+
 ;; =================
 ;; Generic Procedure
 ;; =================
