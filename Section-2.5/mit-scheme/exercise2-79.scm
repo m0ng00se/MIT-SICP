e472c4ec6e566ad2d7377bd5ca6874ee3e6b1a3f
m0ng00se <sholtz9421@gmail.com>
2017-02-22 21:50:01 -0800

    formatting

--- Section-2.5/mit-scheme/exercise2-79.scm
+++ Section-2.5/mit-scheme/exercise2-79.scm
@@ -70,19 +70,19 @@
       (cons (/ n g) (/ d g))))
   (define (add-rat x y)
     (make-rat (+ (* (numer x) (denom y))
-		  (* (numer y) (denom x)))
-	            (* (denom x) (denom y))))
+		 (* (numer y) (denom x)))
+	      (* (denom x) (denom y))))
   (define (sub-rat x y)
     (make-rat (- (* (numer x) (denom y))
-		  (* (numer y) (denom x)))
-	            (* (denom x) (denom y))))
+		 (* (numer y) (denom x)))
+	      (* (denom x) (denom y))))
   (define (mul-rat x y)
     (make-rat (* (numer x) (numer y))
-	            (* (denom x) (denom y))))
+	      (* (denom x) (denom y))))
   (define (div-rat x y)
     (make-rat (* (numer x) (denom y))
-	            (* (denom x) (numer y))))
-
+	      (* (denom x) (numer y))))
+  
   ;; interface to rest of the system
   (define (tag x) (attach-tag 'rational x))
   (put 'add '(rational rational)
@@ -93,7 +93,7 @@
        (lambda (x y) (tag (mul-rat x y))))
   (put 'div '(rational rational)
        (lambda (x y) (tag (div-rat x y))))
-
+  
   ;; ============= 
   ;; New Procedure
   ;; ============= 
@@ -101,7 +101,7 @@
        (lambda (x y)
 	 (and (= (numer x) (numer y))
 	      (= (denom x) (denom y)))))
-
+  
   (put 'make 'rational
        (lambda (n d) (tag (make-rat n d))))
   'done)
@@ -122,21 +122,21 @@
     ((get 'make-from-real-imag 'rectangular) x y))
   (define (make-from-mag-ang r a)
     ((get 'make-from-mag-ang 'polar) r a))
-
+  
   ;; internal procedures
   (define (add-complex z1 z2)
     (make-from-real-imag (+ (real-part z1) (real-part z2))
-			  (+ (imag-part z1) (imag-part z2))))
+			 (+ (imag-part z1) (imag-part z2))))
   (define (sub-complex z1 z2)
     (make-from-real-imag (- (real-part z1) (real-part z2))
-			  (- (imag-part z1) (imag-part z2))))
+			 (- (imag-part z1) (imag-part z2))))
   (define (mul-complex z1 z2)
     (make-from-mag-ang (* (magnitude z1) (magnitude z2))
-		              (+ (angle z1) (angle z2))))
+		       (+ (angle z1) (angle z2))))
   (define (div-complex z1 z2)
     (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
-		              (- (angle z1) (angle z2))))
-
+		       (- (angle z1) (angle z2))))
+  
   ;; interface to rest of the system
   (define (tag z) (attach-tag 'complex z))
   (put 'add '(complex complex)
@@ -162,17 +162,17 @@
 	 (let ((tolerance 0.0000001))
 	   (and (< (diff (real-part x) (real-part y)) tolerance)
 		(< (diff (imag-part x) (imag-part y)) tolerance)))))
-
+  
   (put 'make-from-real-imag 'complex
        (lambda (x y) (tag (make-from-real-imag x y))))
   (put 'make-from-mag-ang 'complex
        (lambda (r a) (tag (make-from-mag-ang r a))))
-
+  
   (put 'real-part '(complex) real-part)
   (put 'imag-part '(complex) imag-part)
   (put 'magnitude '(complex) magnitude)
   (put 'angle '(complex) angle)
-
+  
   'done)
 
 ;;
