3a90215d063f60903bd16e88b2c0d9c2b283df33
m0ng00se <sholtz9421@gmail.com>
2017-10-12 21:20:43 -0700

    moving into a temp table

--- Section-2.5/mit-scheme/table.scm
+++ Section-2.5/mit-scheme/table.scm
@@ -69,34 +69,3 @@
 ;; ==> #f
 ((records 'lookup-proc) 'c 'a)
 ;; ==> #f
-
-;; Operations Table
-(define operation-table (make-table))
-(define get (operation-table 'lookup-proc))
-(define put (operation-table 'insert-proc!))
-
-;; Type-Raising/Coercion Table
-(define (raise-integer->rational n)
-  (make-rational n 1))
-(define (raise-rational->scheme-number r)
-  (let ((n (car r))
-	(d (cdr r)))
-    (make-scheme-number (exact->inexact (/ n d)))))
-(define (raise-scheme-number->complex n)
-  (make-complex-from-real-imag n 0))
-
-(put 'raise '(integer) raise-integer->rational)
-(put 'raise '(rational) raise-rational->scheme-number)
-(put 'raise '(scheme-number) raise-scheme-number->complex)
-
-(define (raise x) (apply-generic 'raise x))
-
-;; Coercion Table
-(define coercion-table (make-table))
-(define get-coercion (coercion-table 'lookup-proc))
-(define put-coercion (coercion-table 'insert-proc!))
-
-(define (scheme-number->complex n)
-  (make-complex-from-real-imag (contents n) 0))
-
-(put-coercion 'scheme-number 'complex scheme-number->complex)
